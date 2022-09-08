bb29166757fee4669df2612719de49f03ab3f251
Junior <35709152+git-jr@users.noreply.github.com>
2022-09-07 21:28:24 -0300

    Criando item de produto

--- app/src/main/java/com/paradoxo/aluvery/MainActivity.kt
+++ app/src/main/java/com/paradoxo/aluvery/MainActivity.kt
@@ -6,16 +6,31 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.paradoxo.aluvery.ui.components.CustomLayoutPreview
 import com.paradoxo.aluvery.ui.theme.AluveryTheme
+import com.paradoxo.aluvery.ui.theme.Purple200
+import com.paradoxo.aluvery.ui.theme.Purple500
+import com.paradoxo.aluvery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,20 +49,57 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProdutoItem() {
-    Column {
-        Box(
-            modifier = Modifier
-                .height(50.dp)
-                .background(Color.Blue)
-                .width(50.dp)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null
-        )
+    Surface(
+        Modifier.padding(8.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 4.dp
+    ) {
+        Column(
+            Modifier
+                .heightIn(min = 250.dp, max = 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500, Teal200
+                            )
+                        )
+                    )
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize / 2)
+                        .clip(shape = CircleShape)
+                        .align(BottomCenter)
+                )
 
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(20).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 14,20",
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(8.dp),
+                    fontWeight = FontWeight(400)
+                )
+            }
+        }
     }
 
 }
